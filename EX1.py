edec14eaed2a42962529164739a9dc05cc81ca45
Agatha240706 <124930919+Agatha240706@users.noreply.github.com>
2023-05-26 08:56:34 -0300

    Add files via upload

---
+++ EX1.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Coleque a nota aqui:"))
+nota2 = float(input("Coloque a nota aqui:"))
+
+media = (nota1+nota2)/2
+
+if media<6:
+    print("Você foi reprovado")
+if media>=6:
+    print("Você foi aprovado")
